757b6e2c3ffb455eeafeadff1889507530d68e23
Ryan C. Durham <rydurham@gmail.com>
2014-09-21 11:14:03 -0500

    Updated Parleyable Trait

--- src/Parley/Traits/Parleyable.php
+++ src/Parley/Traits/Parleyable.php
@@ -4,18 +4,9 @@ use SRLabs\Parley\Models\Thread;
 
 trait Parleyable {
 
-    public function parley( $creator ) {
-        $thread = new Thread();
-        $thread->addMember($this);
-        $thread->addMember($creator);
-        return $thread;
-    }
+    public function notify()
+    {
 
-    public function getParleys() {
-        return Thread::join('parley_members', 'parley_threads.id', '=', 'parley_members.parley_thread_id')
-                ->where('parley_members.parleyable_id', $this->id)
-                ->where('parley_members.parleyable_type', $this->getModel())
-                ->get();
     }
 
 
