54477cfc28b6da59631ec335325f16c691326757
Ryan C. Durham <rydurham@gmail.com>
2014-09-23 12:59:10 -0500

    Update tests for new Selector API

--- tests/SupportTests.php
+++ tests/SupportTests.php
@@ -147,12 +147,12 @@ class SupportTests extends \Orchestra\Testbench\TestCase {
 
         $thread4->markReadForMember( $user1 );
 
-        $user1Threads            = \Parley::gather('all')->belongingTo($user1)->get();
-        $user1OpenThreads        = \Parley::gather('open')->belongingTo($user1)->get();
-        $user1ClosedThreads      = \Parley::gather('closed')->belongingTo($user1)->get();
-        $user1ThreadsWithTrashed = \Parley::gather('all')->withTrashed()->belongingTo($user1)->get();
-        $user1ThreadsOnlyTrashed = \Parley::gather('all')->onlyTrashed()->belongingTo($user1)->get();
-        $multiGatherThreads      = \Parley::gather('all')->belongingTo([$user1, $group])->get();
+        $user1Threads            = \Parley::gather()->belongingTo($user1)->get();
+        $user1OpenThreads        = \Parley::gatherOpen()->belongingTo($user1)->get();
+        $user1ClosedThreads      = \Parley::gatherClosed()->belongingTo($user1)->get();
+        $user1ThreadsWithTrashed = \Parley::gather()->withTrashed()->belongingTo($user1)->get();
+        $user1ThreadsOnlyTrashed = \Parley::gather()->onlyTrashed()->belongingTo($user1)->get();
+        $multiGatherThreads      = \Parley::gather()->belongingTo([$user1, $group])->get();
 
         $this->assertEquals(2, $user1Threads->count());
         $this->assertEquals(1, $user1OpenThreads->count());
@@ -164,11 +164,54 @@ class SupportTests extends \Orchestra\Testbench\TestCase {
         $this->assertEquals(3, $multiGatherThreads->unread());
     }
 
+    public function testGetMemberThreadCounts()
+    {
+        $user1 = User::create(['email' => 'test1@test.com', 'first_name' => 'Test', 'last_name' => 'User']);
+        $user2 = User::create(['email' => 'test2@test.com', 'first_name' => 'Another', 'last_name' => 'User']);
+        $group = Group::create(['name' => 'testGroup']);
+
+        $thread1 = \Parley::discuss('Test Thread 1')->amongst([$user1, $user2])->message([
+            'body'   => "This is one Thread",
+            'alias'  => $user1->first_name . ' ' . $user1->last_name,
+            'author' => $user1
+        ]);
+
+        $thread2 = \Parley::discuss('Test Thread 2')->amongst([$user1, $user2])->message([
+            'body'   => "This is another Thread",
+            'alias'  => $user1->first_name . ' ' . $user1->last_name,
+            'author' => $user1
+        ]);
+
+        $thread2->markReadForMember($user1);
+
+        $thread3 = \Parley::discuss('Test Thread 3')->amongst([$user1, $user2])->message([
+            'body'   => "This thread will be 'deleted'",
+            'alias'  => $user1->first_name . ' ' . $user1->last_name,
+            'author' => $user1
+        ]);
+
+        $thread3->delete();
+
+        $user1All  = \Parley::gather()->belongingTo($user1)->count();
+        $user1Unread = \Parley::gather()->unread()->belongingTo($user1)->count();
+        $user1Read = \Parley::gather()->read()->belongingTo($user1)->count();
+
+        $user2Unread = \Parley::gather()->unread()->belongingTo($user2)->count();
+        $user2Read = \Parley::gather()->read()->belongingTo($user2)->count();
+
+        $this->assertEquals(2, $user1All);
+        $this->assertEquals(1, $user1Unread);
+        $this->assertEquals(1, $user1Read);
+        $this->assertEquals(2, $user2Unread);
+        $this->assertEquals(0, $user2Read);
+    }
+
+
     public function testGatherThreadsForInvalidMemberObject()
     {
         $group = null;
 
-        $threads = \Parley::gather('all')->belongingTo($group)->get();
+        $threads = \Parley::gather()->belongingTo($group)->get();
 
         $this->assertInstanceOf('SRLabs\Parley\Support\Collection', $threads);
         $this->assertEquals(0, $threads->count());
