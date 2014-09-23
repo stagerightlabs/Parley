74be23b6f7aaee3b2cf25533f83bbedb25aace81
Ryan C. Durham <rydurham@gmail.com>
2014-09-22 21:03:19 -0500

    Collection and Selector classes

---
+++ src/Parley/Support/Collection.php
@@ -0,0 +1,21 @@
+<?php namespace SRLabs\Parley\Support;
+
+
+class Collection extends \Illuminate\Support\Collection {
+
+   public function unread()
+   {
+       $count = 0;
+
+       foreach ($this->items as $thread)
+       {
+            if ($thread->is_read == 0)
+            {
+                $count++;
+            }
+       }
+
+       return $count;
+   }
+
+} 
